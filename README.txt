e5b2514d7e4c32482534edd953c8a38f782ca6b4
GradXL49 <gradxl1573@gmail.com>
2022-05-05 21:47:16 -0400

    Added functionality to testing where it handles different device types from moddel load.

--- README.txt
+++ README.txt
@@ -4,6 +4,6 @@ Run training.py to generate Generator and Discriminator models. Swap lines 43, 4
 with the commented code on the same line to switch between training on MNIST or KMNIST
 
 Run testing.py to generate examples and test them on the discriminator model. Replace lines
-12 and 14 with the commented code on the same line to switch between MNIST and KMNIST models.
+18 and 20 with the commented code on the same line to switch between MNIST and KMNIST models.
 Change the value of n on line 23 to set the number of test batches to perform in one run and
 change the value of batch_size on line 24 to change the number of generated images in each batch.
--- testing.py
+++ testing.py
@@ -8,19 +8,20 @@ import torch
 from math import sqrt
 from matplotlib import pyplot as plt
 
-# load models
-generator = torch.load('MNIST Model/Generator_epoch_149.pth') #torch.load('KMNIST Model/Generator_epoch_149.pth')
-print(generator.eval())
-discriminator = torch.load('MNIST Model/Discriminator_epoch_149.pth') #torch.load('KMNIST Model/Discriminator_epoch_149.pth')
-print(discriminator.eval())
 
 # determine if gpu is available
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 print(device)
 device = torch.device(device)
 
+# load models
+generator = torch.load('MNIST Model/Generator_epoch_149.pth', map_location=device) #torch.load('KMNIST Model/Generator_epoch_149.pth', map_location=device)
+print(generator.eval())
+discriminator = torch.load('MNIST Model/Discriminator_epoch_149.pth', map_location=device) #torch.load('KMNIST Model/Discriminator_epoch_149.pth', map_location=device)
+print(discriminator.eval())
+
 # hyperparameter settings
-n = 1
+n = 10
 batch_size = 64
 sqr = sqrt(batch_size)
 if sqr / int(sqr) != 1:
@@ -46,7 +47,7 @@ for i in range(n):
 
     # feed to discriminator and do testing
     confidence = str(discriminator(generated_samples))
-    confidence = confidence.replace('tensor([', '').replace("], device='cuda:0', grad_fn=<SigmoidBackward0>)", '').replace('[', '').replace(']', '').replace('\n', '').replace('\t', '').replace(' ', '')
+    confidence = confidence.replace('tensor([', '').replace("], device='cuda:0', grad_fn=<SigmoidBackward0>)", '').replace("], grad_fn=<SigmoidBackward0>)", '').replace('[', '').replace(']', '').replace('\n', '').replace('\t', '').replace(' ', '')
     print("\nBatch "+str(i+1)+" Confidences:\n"+confidence)
     conf = confidence.split(',')
     avg = 0
